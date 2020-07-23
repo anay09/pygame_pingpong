221a5792555f1e9659608a69c3448d5b198fd3e0
anayc <anay.chopade>
2020-07-23 20:25:32 +0100

    Created the object Ball within a new python file ball.py and implemented this object in the game interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from paddle import Paddle
+from ball import Ball
 
 pygame.init()
 
@@ -20,10 +21,16 @@ paddleB = Paddle(WHITE, 10, 100)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
 
+ball = Ball(WHITE,10,10)
+ball.rect.x = 345
+ball.rect.y = 195
+
 all_sprites_list = pygame.sprite.Group()
 
 all_sprites_list.add(paddleA)
 all_sprites_list.add(paddleB)
+all_sprites_list.add(ball)
+
 
 carryOn = True
 
@@ -49,6 +56,18 @@ while carryOn:
 
     all_sprites_list.update()
 
+    if ball.rect.x >= 690:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x <= 0:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y > 490:
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y < 0:
+        ball.velocity[1] = -ball.velocity[1]
+
+    if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
+        ball.bounce()
+
     screen.fill(BLACK)
     pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
 
@@ -57,6 +76,5 @@ while carryOn:
     pygame.display.flip()
 
     clock.tick(60)
-
 
 pygame.quit()
